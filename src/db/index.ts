1ebd944dfbfdc3e07f7670189775111bceb2e64b
aravm256-8094 <aravm256@gmail.com>
2026-05-11 17:26:46 -0400

    Fix DATABASE_URL fallback env var

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import * as schema from './schema';
 
-const connectionString = process.env.DATABASE_URL!;
+const connectionString = process.env.DATABASE_URL || process.env.COCKROACHDB_CONNECTION_STRING!;
 const sqlClient = postgres(connectionString);
 export const db = drizzle(sqlClient, { schema });
 
